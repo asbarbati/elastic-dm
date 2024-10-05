3080e38c0b210149939a6efafec3769cd1b2fbba
Alessandro Sbarbati <122787746+asbarbati@users.noreply.github.com>
2024-10-05 23:34:25 +0200

    chore: Adding Github test integration

--- tests/assets/load_fixtures.bash
+++ tests/assets/load_fixtures.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+READY=0
+until [[ ${READY} == "1" ]]; do
+    curl -X GET https://127.0.0.1:9200 -k -u elastic:elastic > /dev/null 2>&1 
+    if [[ $? == "0" ]]; then
+        READY="1"
+    fi
+    sleep 1
+done
+
+echo "Elasticsearch Ready."
+
 curl -u elastic:elastic -k -X POST "https://localhost:9200/_bulk?pretty" -H 'Content-Type: application/json' -d'
 {"index" : {"_index" : "elastic-dm-test", "_id" : "jTzhrZABVFAGNiUIDqjU" } }
 {"name":"Foobar","last":"Bar","address":"","ip":"1.1.1.1","email":"info@example.com","epoch":1582889588.3967564,"words":["property","truth","across"]}
